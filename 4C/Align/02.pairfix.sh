ef3fa3f42e5f2502fb5ab14711f6148a6d9f1d50
lokapal <lokapal@gmail.com>
2023-05-24 01:05:52 +0300

    Update 02.pairfix.sh

--- 4C/Align/02.pairfix.sh
+++ 4C/Align/02.pairfix.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 TYPE=white
 REP=rep1
-repair.sh -Xmx50g in1=R5.R1.fastq.gz in2=R5.R2.fastq.gz out1=4C.K562.filtered.R1.fastq.gz out2=4C.K562.filtered.R2.fastq.gz outs=4C.K562.filtered.singletons.fastq.gz repair overwrite=t
+repair.sh -Xmx50g in1=$TYPE.$REP.R1.fastq.gz in2=$TYPE.$REP.R2.fastq.gz \
+out1=4C.$TYPE.$REP.filtered.R1.fastq.gz out2=4C.$TYPE.$REP.filtered.R2.fastq.gz \
+outs=4C.K562.filtered.singletons.fastq.gz repair overwrite=t
