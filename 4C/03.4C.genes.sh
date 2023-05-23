8d0c31a1858c7f49035c428ec69b4b7a04c846fc
lokapal <lokapal@gmail.com>
2023-05-23 16:57:59 +0300

    Update and rename 4C/white.rep1/03.4C.genes.sh to 4C/03.4C.genes.sh

--- 4C/03.4C.genes.sh
+++ 4C/03.4C.genes.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # script to find intersections between repicates, to filter DFAM, and to assign intersections to genes
 # and their expression values 
 #
@@ -22,9 +22,11 @@
 #
 # Part 1. Find intersection between replicates, remove mappings that are completely inside DFAM entries
 # get DFAM database and convert it to bed format
+# set type
+TYPE=white
 wget https://www.dfam.org/releases/current/annotations/hg38/hg38.nrph.hits.gz
 ./lib/dfam2bed.pl hg38.nrph.hits.gz
-mv hg38.nrph.hits.bed  hg38_dfam.bed
+mv hg38.nrph.hits.bed hg38_dfam.bed
 sort -k1,1 -k2,2n -k3,3n hg38_dfam.bed -o hg38_dfam.bed
 #
 cp table_hg38.rep1.txt rep1.txt
@@ -40,19 +42,19 @@ bedtools intersect -a 4C_hg38_nodfam.rep1.bedGraph -b 4C_hg38_nodfam.rep2.bedGra
 sort -k1,1 -k2,2n -k3,3n intersect_reps.bedGraph -o intersect_reps.bedGraph
 sort -k1,1 -k2,2n -k3,3n intersect_reps.bed -o intersect_reps.bed
 bedtools merge -c 4 -o mean -i intersect_reps.bed > intersect_merged.bed
-./lib/mergebedgraphmean.sh intersect_reps.bedGraph K562.4C.white.nodfam.intersect.bedGraph
-./lib/addSubseq.pl /usr/local/genomes/hg38.mfa intersect_merged.bed > K562.4C.white.nodfam.intersect.txt
-sort -k1,1 -k2,2n -k3,3n K562.4C.white.nodfam.intersect.txt -o K562.4C.white.nodfam.intersect.txt
+./lib/mergebedgraphmean.sh intersect_reps.bedGraph K562.4C.$TYPE.nodfam.intersect.bedGraph
+./lib/addSubseq.pl /usr/local/genomes/hg38.mfa intersect_merged.bed > K562.4C.$TYPE.nodfam.intersect.txt
+sort -k1,1 -k2,2n -k3,3n K562.4C.$TYPE.nodfam.intersect.txt -o K562.4C.$TYPE.nodfam.intersect.txt
 #
 # Part 2. Assign genes to K562 4C-associated intersected replicates noDFAM mappings
-cp K562.4C.white.nodfam.intersect.txt 4C.bed
+cp K562.4C.$TYPE.nodfam.intersect.txt 4C.bed
 # Annotation h38.gtf should be in /usr/local/genomes
 Rscript ./lib/getgenes.R
 sort -k1,1 -k2,2n -k3,3n hg38.genes.bed -o hg38.genes.bed
 bedtools intersect -wa -wb -a 4C.bed -b hg38.genes.bed > 4C.genetable
 bedtools intersect -v -wa -a 4C.bed -b hg38.genes.bed > 4C.empty
-./lib/tbl_addexpr.pl 4C.genetable ./lib/K562.white.mean.TPM
-cat 4C.genetable.expr 4C.empty > K562.4C.white.intersect.all.txt
-mv 4C.genetable.expr K562.4C.white.intersect.genes.txt
-sort -k1,1V -k2,2n -k3,3n K562.4C.white.intersect.genes.txt -o K562.4C.white.intersect.genes.txt
-sort -k1,1V -k2,2n -k3,3n K562.4C.white.intersect.all.txt -o K562.4C.white.intersect.all.txt
+./lib/tbl_addexpr.pl 4C.genetable ./lib/K562.$TYPE.mean.TPM
+cat 4C.genetable.expr 4C.empty > K562.4C.$TYPE.intersect.all.txt
+mv 4C.genetable.expr K562.4C.$TYPE.intersect.genes.txt
+sort -k1,1V -k2,2n -k3,3n K562.4C.$TYPE.intersect.genes.txt -o K562.4C.$TYPE.intersect.genes.txt
+sort -k1,1V -k2,2n -k3,3n K562.4C.$TYPE.intersect.all.txt -o K562.4C.$TYPE.intersect.all.txt
