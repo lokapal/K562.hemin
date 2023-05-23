dff8d77e8ce52b9cac2cecf85c3f56e5571d3021
lokapal <lokapal@gmail.com>
2023-05-23 18:09:30 +0300

    Create Diff4C.R

---
+++ 4C/Diff.4C/Diff4C.R
@@ -0,0 +1,157 @@
+#!/usr/bin/Rscript
+## RNA-seq analysis with DESeq2
+
+countdata <- read.table("counts.selected", header=TRUE, row.names=1)
+
+# Remove first five columns (chr, start, end, strand, length)
+countdata <- countdata[ ,6:ncol(countdata)]
+
+# Remove .bam or .sam from filenames
+colnames(countdata) <- gsub("\\.[sb]am$", "", colnames(countdata))
+colnames(countdata) <- gsub("\\.\\.\\.", "", colnames(countdata))
+
+# Convert to matrix
+countdata <- as.matrix(countdata)
+head(countdata)
+
+# Assign condition (first two are expansion, second two are control)
+(condition <- factor(c(rep("ctl", 2), rep("exp", 2))))
+
+# Analysis with DESeq2 ----------------------------------------------------
+
+suppressPackageStartupMessages(library(DESeq2))
+
+# Create a coldata frame and instantiate the DESeqDataSet. See ?DESeqDataSetFromMatrix
+(coldata <- data.frame(row.names=colnames(countdata), condition))
+dds <- DESeqDataSetFromMatrix(countData=countdata, colData=coldata, design=~condition)
+
+vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
+rld <- rlogTransformation(dds,fitType="local")
+hist(assay(rld))
+
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("ggplot2"))
+
+ddc <- estimateSizeFactors(dds)
+
+df <- bind_rows(
+  as_data_frame(log2(counts(ddc, normalized=TRUE)[, 1:2]+1)) %>% mutate(transformation = "log2(x + 1)"),
+  as_data_frame(assay(vsd)[, 1:2]) %>% mutate(transformation = "VarianceStabilizingTransformation"),
+  as_data_frame(assay(rld)[, 1:2]) %>% mutate(transformation = "rLogTransformation (DESeq2 default)"))
+
+colnames(df)[1:2] <- c("x", "y")  
+
+cairo_pdf("K562.4C.scatterplots.pdf",width=15,height=10,antialias="default",fallback_resolution = 300,onefile=T)
+ggplot(df, aes(x = x, y = y)) + geom_hex(bins = 80) + coord_fixed() + facet_grid( . ~ transformation) + labs(x = "WHITE.rep1", y="WHITE.rep2")
+
+rm (df)
+df <- bind_rows(
+  as_data_frame(log2(counts(ddc, normalized=TRUE)[, 3:4]+1)) %>% mutate(transformation = "log2(x + 1)"),
+  as_data_frame(assay(vsd)[, 3:4]) %>% mutate(transformation = "VarianceStabilizingTransformation"),
+  as_data_frame(assay(rld)[, 3:4]) %>% mutate(transformation = "rLogTransformation (DESeq2 default)"))
+
+colnames(df)[1:2] <- c("x", "y")
+ggplot(df, aes(x = x, y = y)) + geom_hex(bins = 80) + coord_fixed() + facet_grid( . ~ transformation) + labs(x="RED.rep1", y="RED.rep2")
+invisible(dev.off())
+
+# Run the DESeq pipeline, fitType="local" for 4Cseq data
+dds <- DESeq(dds,fitType="local")
+
+# Plot dispersions
+cairo_pdf("K562.4C.qc-dispersions.pdf",width=15,height=10,antialias="default",fallback_resolution = 300)
+plotDispEsts(dds, main="Dispersion plot")
+invisible(dev.off())
+
+## Use RColorBrewer, better
+suppressPackageStartupMessages(library(RColorBrewer))
+(mycols <- brewer.pal(8, "Dark2")[1:length(unique(condition))])
+
+# Sample distance heatmap
+sampleDists <- as.matrix(dist(t(assay(rld))))
+suppressPackageStartupMessages(library(gplots))
+cairo_pdf("K562.4C.qc-heatmap.pdf",width=15,height=10,antialias="default",fallback_resolution = 300)
+heatmap.2(as.matrix(sampleDists), key=F, trace="none",
+          col=colorpanel(100, "black", "white"),
+          ColSideColors=mycols[condition], RowSideColors=mycols[condition],
+          margin=c(10, 10), main="Sample Distance Matrix")
+invisible(dev.off())
+
+suppressPackageStartupMessages(require(pcaExplorer))
+cairo_pdf("K562.4C.PCA.pdf",width=15,height=10,antialias="default",fallback_resolution = 300,onefile=T)
+pcaplot(rld,intgroup="condition",title = "RlogStabilizingTransformation 4C (default DESeq2 method)")
+pcaplot(vsd,intgroup="condition",title = "VarianceStabilizingTransformation 4C")
+invisible(dev.off())
+
+# Get differential expression results
+res <- results(dds)
+# decoding gene names
+suppressPackageStartupMessages(require(biomaRt))
+ensembl_ids <- row.names(res) 
+# Prepare gene table with some simple caching to avoid stressing the Ensembl server by many repeated runs 
+genes.table = NULL 
+if (!file.exists("cache.genes.table")) {
+      message("Retrieving genes table from Ensembl...")
+      mart <- useMart("ensembl")
+#listDatasets(mart=mart)
+      mart <- useDataset("hsapiens_gene_ensembl", mart = mart)
+      genetable <- getBM(filter="ensembl_gene_id", attributes=c("ensembl_gene_id", "external_gene_name"), values=ensembl_ids, mart=mart,uniqueRows=TRUE)
+      save(genetable, file= "cache.genes.table")
+                                       } else {
+        load("cache.genes.table")
+        message("Reading gene annotation information from cache file,\nRemove the file cache.genes.table if you want to force retrieving data from Ensembl")
+         }
+
+m <- match(genetable$ensembl_gene_id, rownames(res))
+res.sub <- res[m,]
+res.sub$symbol <- genetable$external_gene_name
+mcols(res.sub)[7,] <- DataFrame(type="GeneName",description="Common Gene Name")
+#mcols(res.sub)
+res <- res.sub
+table(res$padj<0.05)
+## Order by adjusted p-value
+res <- res[order(res$padj), ]
+## Merge with normalized count data
+resdata <- merge(as.data.frame(res), as.data.frame(counts(dds, normalized=TRUE)), by="row.names", sort=FALSE)
+names(resdata)[1] <- "Gene"
+## Write results
+write.table(resdata, file="diffexpr-results.tsv",sep='\t')
+
+cairo_pdf("K562.4C.maplot.pdf",width=15,height=10,pointsize=16,antialias="default",fallback_resolution = 300,onefile=T)
+plotMA(dds, ylim=c(-1,1), cex=1)
+invisible(dev.off())
+
+suppressPackageStartupMessages(library("EnhancedVolcano"))
+
+cairo_pdf("K562.4C.volcanoplot.pdf",width=15,height=10,pointsize=16,antialias="default",fallback_resolution = 300,onefile=T)
+EnhancedVolcano(res,lab=res$symbol,x='log2FoldChange',y='pvalue',
+    xlab = bquote(~Log[2]~ 'fold change'),
+    pCutoff = 0.01,
+    FCcutoff = 2,
+    transcriptLabSize = 4.0,
+    colAlpha = 1,
+    legend=c('NS','Log (base 2) fold-change','P value', 'P value & Log (base 2) fold-change'),
+    legendPosition = 'bottom',
+    legendLabSize = 10,
+    legendIconSize = 3.0,
+#    drawConnectors = TRUE,
+#    widthConnectors = 0.2,
+#    colConnectors = 'grey30',
+) + coord_cartesian(xlim=c(-21,21),ylim=c(0,300)) + scale_x_continuous(breaks=seq(-25,30,5), minor_breaks=seq(-26,26,1)) + scale_y_continuous(breaks=seq(0,300,50),minor_breaks=seq(1,500,10))
+
+EnhancedVolcano(res,lab=res$symbol,x='log2FoldChange',y='padj',
+    xlab = bquote(~Log[2]~ 'fold change'),
+    ylab = bquote(~-Log[10]~adjusted~italic(P)),
+    pCutoff = 0.01,
+    FCcutoff = 2,
+    transcriptLabSize = 4.0,
+    colAlpha = 1,
+    legend=c('NS','Log2 FC','Adjusted p-value', 'Adjusted p-value & Log2 FC'),
+    legendPosition = 'bottom',
+    legendLabSize = 10,
+    legendIconSize = 3.0,
+#    drawConnectors = TRUE,
+#    widthConnectors = 0.2,
+#    colConnectors = 'grey30'
+) + coord_cartesian(xlim=c(-21,21),ylim=c(0,300)) + scale_x_continuous(breaks=seq(-25,30,5), minor_breaks=seq(-26,26,1)) + scale_y_continuous(breaks=seq(0,300,50),minor_breaks=seq(1,500,10))
+
+invisible(dev.off())
