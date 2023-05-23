2206f7f6627b7208314fdfa4557a83a1573ebb73
lokapal <lokapal@gmail.com>
2023-05-23 18:58:34 +0300

    Update 01.cut.R1.sh

--- 4C/Align/01.cut.R1.sh
+++ 4C/Align/01.cut.R1.sh
@@ -4,7 +4,7 @@ REP=rep1
 # leading full adapters at 5' end cut
 cutadapt -O 10 -j 20 --trim-n --times=4 --minimum-length 20 -q 24 --untrimmed-output=untr.R1.1.fastq.gz \
 -g file:./lib/fulladapters.fa \
--o R1.1.fastq.gz 4C.K562.minus.rep1.R1.fastq.gz
+-o R1.1.fastq.gz 4C.K562.$TYPE.$REP.R1.fastq.gz
 mv R1.1.fastq.gz R1.j1.fastq.gz
 #cat untr.R1.1.fastq.gz 
 #
@@ -26,4 +26,5 @@ cat untr.R4.1.fastq.gz R4.1.fastq.gz  > R4.j1.fastq.gz
 cutadapt -O 10 -j 20 --times=4 --minimum-length 20 -q 24 --untrimmed-output=untr.R5.1.fastq.gz \
 -a file:./lib/fulladapters.tails.fa \
 -o R5.1.fastq.gz R4.j1.fastq.gz
-cat untr.R5.1.fastq.gz R5.1.fastq.gz  > R5.j1.fastq.gz
+cat untr.R5.1.fastq.gz R5.1.fastq.gz  > R1.fastq.gz
+# second step - we will try to find 4C adapters in UNTRIMMED reads
