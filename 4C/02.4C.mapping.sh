3dea2842b6d660a50eed4be83e6b87ce065bd8df
lokapal <lokapal@gmail.com>
2023-05-23 17:02:47 +0300

    Update 02.4C.mapping.sh

--- 4C/02.4C.mapping.sh
+++ 4C/02.4C.mapping.sh
@@ -3,8 +3,8 @@
 # (C) Yuri Kravatsky, lokapal@gmail.com, jiri@eimb.ru
 # Input:  1. GSE232393: GSM7329867, GSM7329867 	K562 untreated 4C-rDNA reads after adapter removing
 # Output: 1. table_hg38.white.rep1.txt the file with the genome-wide hg38 DSB mappings: chromosome coordinates, alignment length, coverage, reads, sequence
-#         2. .bam       sorted alignment file that doesn't contain unaligned reads for first replicate
-#            rep1.bam.bai   index file for rep1.bam
+#         2. white.rep1.bam       sorted alignment file that doesn't contain unaligned reads
+#            white.rep1.bam.bai   index file for white.rep1.bam
 #
 # Dependency tools:
 # 1. bwa                  http://bio-bwa.sourceforge.net/
@@ -16,13 +16,14 @@
 TYPE=white
 # set replicate number before use. rep1 for the first replicate, rep2 for the second etc.
 REP=rep1
+#
 bwa mem -t 20 /usr/local/genomes/hg38.mfa $TYPE.$REP.R1.fastq.gz $TYPE.$REP.R2.fastq.gz | samtools view -bS -F 4 -o hits.bam
 samtools sort -@ 10 hits.bam -O BAM -o hits_sorted.bam
 mv hits_sorted.bam hits.bam
 samtools mpileup -f /usr/local/genomes/hg38.mfa hits.bam -o pileup.txt
 samtools view -@ 10 -O SAM hits.bam -o hits.sam
-mv hits.bam $REP.bam
-samtools index -@ 10 $REP.bam
+mv hits.bam $TYPE.$REP.bam
+samtools index -@ 10 $TYPE.$REP.bam
 sort -k1,1 -k2,2n pileup.txt | bgzip -c > compressed.pileup.gz
 tabix -s 1 -b 2 -e 2 compressed.pileup.gz
 perl ./lib/makeTablePileup.pl pileup.txt > table.txt
