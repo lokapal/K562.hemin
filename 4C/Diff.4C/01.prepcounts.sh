b9625c097a1ed78f5c7ea0dff0975d8e7a44ec92
lokapal <lokapal@gmail.com>
2023-05-23 18:27:19 +0300

    Create 01.prepcounts.sh

---
+++ 4C/Diff.4C/01.prepcounts.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+featureCounts -p -a /usr/local/genomes/hg38.gtf -o counts.txt -T 12 -t gene -g gene_id -M --readExtension5 2500 --readExtension3 2500 \
+..\Align\rep1.white.bam ..\Align\rep2.white.bam ..\Align\rep1.red.bam ..\Align\rep2.red.bam
